8de956f3f04c506ebcfb966256c551ca5f4653e9
John Cashmore <john@cashmo.re>
2018-05-31 10:45:48 +0100

    fix: update linting issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ChildComponent } from './child/child.component';
+import { ACChildComponent } from './child/child.component';
 import { AngularContentPortalModule } from './modules/portal/portal.module';
 
 @NgModule({
-  declarations: [AppComponent, ChildComponent],
+  declarations: [AppComponent, ACChildComponent],
   imports: [BrowserModule, AngularContentPortalModule],
   exports: [AngularContentPortalModule],
   providers: [],
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-child',
+  selector: 'ac-app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit {
+export class ACChildComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
