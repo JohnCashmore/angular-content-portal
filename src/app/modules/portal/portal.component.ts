35508b824193a46d673b5e94210745b5f77c6878
John Cashmore <john@cashmo.re>
2019-02-16 17:04:26 +0000

    Add prettier to project

--- src/app/modules/portal/portal.component.ts
+++ src/app/modules/portal/portal.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit, OnDestroy, ViewChild, TemplateRef, Input, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  ViewChild,
+  TemplateRef,
+  Input,
+  AfterViewInit,
+  ChangeDetectorRef
+} from '@angular/core';
 import { TemplatePortal } from '@angular/cdk/portal';
 
 import { ACPortalService } from './portal.service';
@@ -7,22 +16,28 @@ import { ACPortalService } from './portal.service';
   selector: 'ac-portal',
   template: `
     <ng-template #portal>
-        <ng-content></ng-content>
+      <ng-content></ng-content>
     </ng-template>
-    `
+  `
 })
 export class ACPortalComponent implements OnDestroy, AfterViewInit {
   @Input() name: string = null;
 
   @ViewChild('portal') portal: TemplateRef<any>;
 
-  constructor(public portalService: ACPortalService, private cd: ChangeDetectorRef) {
+  constructor(
+    public portalService: ACPortalService,
+    private cd: ChangeDetectorRef
+  ) {
     portalService.resetPortal(this.name);
   }
 
   ngAfterViewInit() {
     setTimeout(() => {
-      this.portalService.setPortal(new TemplatePortal(this.portal, undefined, {}), this.name);
+      this.portalService.setPortal(
+        new TemplatePortal(this.portal, undefined, {}),
+        this.name
+      );
     });
   }
 
